d2d8efa7a478e3636fabecd305491f2786720cb6
7PPi <tchipoqueemanuel@gmail.com>
2026-09-07 23:47:34 +0100

    fruit

--- src/fruit.py
+++ src/fruit.py
@@ -4,19 +4,18 @@ from constants import *
 from sprites import FruitSprites
 
 class Fruit(Entity):
-	def __init__(self, node):
-		super().__init__(node)
-		self.name = FRUIT
-		self.color = GREEN
-		self.lifespan = 5
-		self.timer = 0
-		self.destroy = False
-		self.points = 100
-		self.setBetweenNodes(RIGHT)
-		self.sprites = FruitSprites(self)
+		def __init__(self, node, level=0):
+				Entity.__init__(self, node)
+				self.name = FRUIT
+				self.color = GREEN
+				self.lifespan = 5
+				self.timer = 0
+				self.destroy = False
+				self.points = 100 + level*20
+				self.setBetweenNodes(RIGHT)
+				self.sprites = FruitSprites(self, level)
 
-
-	def update(self, dt):
-		self.timer += dt
-		if self.timer >= self.lifespan:
-			self.destroy = True
+		def update(self, dt):
+				self.timer += dt
+				if self.timer >= self.lifespan:
+						self.destroy = True
